b5f1a4c1b6744e2a6bd8049d515b61041570918a
Umqra <golovoiapklavu@gmail.com>
2017-02-11 16:47:55 +0500

    Remove async from stub storage class

--- Internship.Task/Storage/SimpleStorage.cs
+++ Internship.Task/Storage/SimpleStorage.cs
@@ -7,8 +7,9 @@ namespace Internship.Storage
 {
     public class SimpleStorage : IStatisticStorage
     {
-        private Dictionary<Tuple<string, DateTime>, MatchInfo> matchInfo;
+        private readonly Dictionary<Tuple<string, DateTime>, MatchInfo> matchInfo;
         private readonly Dictionary<string, ServerInfo> serverInfo;
+        private static Task EmptyTask => Task.FromResult(0);
 
         public SimpleStorage()
         {
@@ -16,33 +17,35 @@ namespace Internship.Storage
             matchInfo = new Dictionary<Tuple<string, DateTime>, MatchInfo>();
         }
 
-        public async Task UpdateServerInfo(string serverName, ServerInfo info)
+        public Task UpdateServerInfo(string serverName, ServerInfo info)
         {
             serverInfo[serverName] = info;
+            return EmptyTask;
         }
 
-        public async Task<ServerInfo> GetServerInfo(string serverName)
+        public Task<ServerInfo> GetServerInfo(string serverName)
         {
             ServerInfo info;
             serverInfo.TryGetValue(serverName, out info);
-            return info;
+            return Task.FromResult(info);
         }
 
-        public async Task UpdateMatchInfo(string serverName, DateTime endTime, MatchInfo match)
+        public Task UpdateMatchInfo(string serverName, DateTime endTime, MatchInfo match)
         {
             matchInfo[Tuple.Create(serverName, endTime)] = match;
+            return EmptyTask;
         }
 
-        public async Task<MatchInfo> GetMatchInfo(string serverName, DateTime endTime)
+        public Task<MatchInfo> GetMatchInfo(string serverName, DateTime endTime)
         {
             MatchInfo info;
             matchInfo.TryGetValue(Tuple.Create(serverName, endTime), out info);
-            return info;
+            return Task.FromResult(info);
         }
 
-        public async Task<IEnumerable<ServerInfo>> GetAllServersInfo()
+        public Task<IEnumerable<ServerInfo>> GetAllServersInfo()
         {
-            return serverInfo.Values;
+            return Task.FromResult((IEnumerable<ServerInfo>)serverInfo.Values);
         }
     }
 }
