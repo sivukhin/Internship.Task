37a8869a8e74c9089b6371a0380884d76d62de7b
Umqra <golovoiapklavu@gmail.com>
2017-03-02 17:30:59 +0500

    Start implementation of StatCore

---
+++ Internship.Task/Storage/StatVariable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataCore;
+using StatCore;
+
+namespace StatisticServer.Storage
+{
+    public class ServerStatistic
+    {
+        public GroupStat<string, MatchInfo, int> TotalMatchesPlayed =
+            StatFor<MatchInfo>.Group(match => match.HostServer.ServerId).Count();
+
+        public GroupStat<string, MatchInfo, int> MaximumPopulation =
+            StatFor<MatchInfo>.Group(match => match.HostServer.ServerId).Max(match => match.Scoreboard.Count);
+    }
+}
